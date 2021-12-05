38d8a7f65a6d23045d6d8c3e6f61887a84c81fe5
Remco Huizenga <huizengagr@gmail.com>
2021-12-05 12:18:54 +0100

    day 5. very slow, but quite clean

---
+++ 2021/Day05.cs
@@ -0,0 +1,53 @@
+﻿using Core;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2021
+{
+    public class Day05 : Day<int>
+    {
+        private Regex AllDigits = new Regex(@"(\d+)");
+
+        public Day05() : base(2021, "day05") { }
+
+        public override int PartOne()
+        {
+            var points = GetPoints();
+            return points.ToHashSet(new PointEqualityComparer())
+                .ToDictionary(p => p, p => points.Count(point => point.X == p.X && point.Y == p.Y))
+                .Count(point => point.Value >= 2);
+        }
+
+        public override int PartTwo()
+        {
+            var points = GetPoints(true);
+            return points.ToHashSet(new PointEqualityComparer())
+                .ToDictionary(p => p, p => points.Count(point => point.X == p.X && point.Y == p.Y))
+                .Count(point => point.Value >= 2);
+        }
+
+        private IEnumerable<Point> GetPoints(bool includeDiagonal = false)
+        {
+            return Input
+                .Select(line => AllDigits.Matches(line).Select(match => int.Parse(match.Value)).ToArray())
+                .Where(points => includeDiagonal || (points[0] == points[2] || points[1] == points[3]))
+                .SelectMany(points => Geometry.GetPointsOnLine(points[0], points[1], points[2], points[3]));
+        }
+    }
+
+    public class PointEqualityComparer: IEqualityComparer<Point>
+    {
+        public bool Equals(Point p1, Point p2)
+        {
+            return p1.X == p2.X && p1.Y == p2.Y;
+        }
+
+        public int GetHashCode([DisallowNull] Point p)
+        {
+            int hCode = p.X ^ p.Y;
+            return hCode.GetHashCode();
+        }
+    }
+}
---
+++ Core/Geometry.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2021
+{
+    public static class Geometry
+    {
+        /// <summary>
+        /// Bresenham's Line Algorithm for finding all points on a line.
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <param name="y0"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <returns></returns>
+        public static IEnumerable<Point> GetPointsOnLine(int x0, int y0, int x1, int y1)
+        {
+            var dx = Math.Abs(x1 - x0);
+            var sx = x0 < x1 ? 1 : -1;
+            var dy = -Math.Abs(y1 - y0);
+            var sy = y0 < y1 ? 1 : -1;
+            var error = dx + dy;
+
+            while (true)
+            {
+                yield return new Point(x0, y0);
+
+                if (x0 == x1 && y0 == y1) yield break;
+                var e2 = 2 * error;
+                if (e2 >= dy)
+                {
+                    error += dy;
+                    x0 += sx;
+                }
+                if (e2 <= dx)
+                {
+                    error += dx;
+                    y0 += sy;
+                }
+            }
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
