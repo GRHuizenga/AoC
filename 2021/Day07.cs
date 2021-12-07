4de0dc85514511cae4bc0f4b46c7c4c88ca91e8b
Remco Huizenga <r.huizenga@sigmax.nl>
2021-12-07 09:24:47 +0100

    day 7

---
+++ 2021/Day07.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2021
+{
+    public class Day07 : Day<int>
+    {
+        private IEnumerable<int> CrabPositions;
+        private int Median;
+
+        public Day07() : base(2021, "day07")
+        {
+            CrabPositions = Input.First().Split(',').Select(position => int.Parse(position)).OrderBy(p => p);
+            Median = CrabPositions.ToArray()[CrabPositions.Count() / 2];
+        }
+
+        public override int PartOne()
+        {
+            // min distance point is median in the list
+            return CrabPositions.Sum(p => Math.Abs(Median - p));
+        }
+
+        public override int PartTwo()
+        {
+            // take median as a starting point
+            var distance = CrabPositions.Sum(p => ((1 + Math.Abs(Median - p)) * Math.Abs(Median - p)) / 2);
+            var minCost = int.MaxValue;
+            while (minCost > distance)
+            {
+                Median++;
+                minCost = distance;
+                distance = CrabPositions.Sum(p => ((1 + Math.Abs(Median - p)) * Math.Abs(Median - p)) / 2);
+            }
+            return minCost;
+        }
+    }
+}
