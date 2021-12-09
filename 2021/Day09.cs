d7773913d3c4903ef397c71c6e9b2653b7a46d9c
Remco Huizenga <huizengagr@gmail.com>
2021-12-09 23:03:29 +0100

    day 9...

--- 2021/Day09.cs
+++ 2021/Day09.cs
@@ -2,31 +2,74 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2021
 {
     public class Day09 : Day<int>
     {
-        private IEnumerable<IEnumerable<int>> HeightMapPadded;
+        private int[] HeightMap;
+        private IEnumerable<int> Sinks;
+        private int Size;
 
-        public Day09() : base(2021, "test")
+        public Day09() : base(2021, "day09")
         {
-            HeightMapPadded = Input
-                .Select(heights => heights.Split().Select(h => int.Parse(h)).Prepend(10).Append(10))
-                .Transpose()
-                .Select(heights => heights.Prepend(10).Append(10));
+            Size = Input.First().Count();
+            HeightMap = Input.SelectMany(c => c.ToCharArray().Select(d => int.Parse(d.ToString()))).ToArray();
+            Sinks = Enumerable.Range(0, HeightMap.Length)
+                .Where(index1D => HeightMap[index1D] < MinNeighbour(index1D).Item2);
         }
 
         public override int PartOne()
         {
-            return -1;
+            return Sinks.Sum(index1D => HeightMap[index1D] + 1);
         }
 
         public override int PartTwo()
         {
-            return -1;
+            var basins = Enumerable.Range(0, HeightMap.Length).Select(c => -1).ToArray();
+            foreach (var sink in Enumerable.Range(0, Sinks.Count())) basins[Sinks.ElementAt(sink)] = sink;
+            foreach (var index1d in Enumerable.Range(0, HeightMap.Length))
+            {
+                if (HeightMap[index1d] != 9)
+                {
+                    basins[index1d] = FindSinkRecursive(basins, index1d);
+                }
+            }
+            
+            return basins
+                .GroupBy(b => b)
+                .Where(x => x.Key != -1)
+                .OrderByDescending(b => b.Count())
+                .Take(3)
+                .Aggregate(1, (acc, basin) => acc * basin.Count());
         }
+
+        private int FindSinkRecursive(int[] basins, int index1d)
+        {
+            return basins[index1d] != -1 ? basins[index1d] : FindSinkRecursive(basins, MinNeighbour(index1d).Item1);
+        }
+
+        private (int index1d, int height) MinNeighbour(int index1d)
+        {
+            return Enum.GetValues(typeof(Direction)).Cast<Direction>()
+                .Select(direction => direction switch
+                {
+                    Direction.North => index1d - Size < 0 ? (index1d, 10) : (index1d - Size, HeightMap[index1d - Size]),
+                    Direction.South => index1d + Size > HeightMap.Length - 1 ? (index1d, 10) : (index1d + Size, HeightMap[index1d + Size]),
+                    Direction.East => (index1d + 1) % Size == 0 ? (index1d, 10) : (index1d + 1, HeightMap[index1d + 1]),
+                    Direction.West => index1d - 1 < 0 || (index1d - 1) % Size == Size - 1 ? (index1d, 10) : (index1d - 1, HeightMap[index1d - 1]),
+                    _ => throw new Exception("This should never happen..."),
+                })
+                .OrderBy(c => c.Item2)
+                .First();
+        }
+    }
+
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West,
     }
 }
