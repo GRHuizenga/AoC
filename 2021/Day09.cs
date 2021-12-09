707077831f0cc3c793a2f550bbae93e2cce91e20
Remco Huizenga <r.huizenga@sigmax.nl>
2021-12-09 17:52:12 +0100

    setup day 9

---
+++ 2021/Day09.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021
+{
+    public class Day09 : Day<int>
+    {
+        private IEnumerable<IEnumerable<int>> HeightMapPadded;
+
+        public Day09() : base(2021, "test")
+        {
+            HeightMapPadded = Input
+                .Select(heights => heights.Split().Select(h => int.Parse(h)).Prepend(10).Append(10))
+                .Transpose()
+                .Select(heights => heights.Prepend(10).Append(10));
+        }
+
+        public override int PartOne()
+        {
+            return -1;
+        }
+
+        public override int PartTwo()
+        {
+            return -1;
+        }
+    }
+}
--- 2021/Program.cs
+++ 2021/Program.cs
@@ -8,7 +8,7 @@ namespace _2021
         {
             try
             {
-                var day = new Day08();
+                var day = new Day09();
                 Console.WriteLine(day.PartOne());
                 Console.WriteLine(day.PartTwo());
                 Console.ReadKey();
