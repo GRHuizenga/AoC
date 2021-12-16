c8f08c7e976ff1a99a53aafcb54eb1463f840ea2
Remco Huizenga <r.huizenga@sigmax.nl>
2021-12-16 16:33:46 +0100

    day 15 part 2

--- 2021/Day15.cs
+++ 2021/Day15.cs
@@ -10,7 +10,7 @@ namespace _2021
 {
     public class Day15 : Day<int>
     {
-        public Day15() : base(2021, "test") { }
+        public Day15() : base(2021, "day15") { }
 
         public override int PartOne()
         {
@@ -22,31 +22,21 @@ namespace _2021
         {
             var block = Input.Select(line => line.ToCharArray().Select(cave => int.Parse(cave.ToString())));
 
-            block = block.Select(row =>
+            var caveSystem = new int[100, 100];
+            var x = 0;
+            var y = 0;
+            foreach (var row in block)
             {
-                var temp = row.ToArray();
-                for (int i = 1; i <= 4; i++)
+                x = 0;
+                foreach (var cave in row)
                 {
-                    row = row.Concat(temp.Select(t => t + i > 9 ? 1 : t + i)).ToList();
+                    caveSystem[y, x] = cave;
+                    x++;
                 }
-                return row;
-            });
+                y++;
+            }
 
-            block = block.Transpose().Select(row =>
-            {
-                var temp = row.ToArray();
-                for (int i = 1; i <= 4; i++)
-                {
-                    row = row.Concat(temp.Select(t => t + i > 9 ? 1 : t + i)).ToList();
-                }
-                return row;
-            }).Transpose();
-
-            var caveSystem = block.SelectMany(row => row).ToArray();
-
-            var x = PathFinding.Dijkstra(caveSystem, 0).distances.Last();
-
-            return -1;
+            return PathFinding.AStarSearch(caveSystem, 0, (500 * 500) - 1);
         }
     }
 }
--- Core/PathFinding.cs
+++ Core/PathFinding.cs
@@ -39,5 +39,47 @@ namespace _2021
                 .OrderBy(pair => pair.Item2)
                 .First().index;
         }
+
+        public static int AStarSearch(int[,] graph, int source, int target)
+        {
+            var xLength = (int)Math.Sqrt(graph.Length);
+            var length = xLength * 5;
+            var queue = new PriorityQueue<int, int>();
+            queue.Enqueue(source, 0);
+
+            var visited = Enumerable.Repeat(false, length * length).ToArray();
+
+            var distances = new Dictionary<int, int>() { [source] = 0 };
+
+            while (queue.TryDequeue(out var current, out int _))
+            {
+                if (visited[current]) continue;
+
+                visited[current] = true;
+
+                if (current == target) return distances[target];
+
+                var cost = distances[current];
+                foreach (var neighbour in current.GetNeighbouringIndices(length, false).Where(n => !visited[n]))
+                {
+                    var nRisk = cost + GetRisk(neighbour % length, neighbour / length) ;
+                    if (nRisk < distances.GetValueOrDefault(neighbour, int.MaxValue))
+                    {
+                        distances[neighbour] = nRisk;
+                        queue.Enqueue(neighbour, nRisk);
+                    }
+                }
+            }
+
+            int GetRisk(int x, int y)
+            {
+                var ydim = y / xLength;
+                var xdim = x / xLength;
+                var value = graph[y % xLength, x % xLength];
+                return ((value - 1 + ydim + xdim) % 9) + 1;
+            }
+
+            return distances[target];
+        }
     }
 }
